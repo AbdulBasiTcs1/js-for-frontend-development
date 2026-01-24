1bbd97d36a2ccf29157ed6625ec96c17ddc9deb7
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-24 09:11:31 +0500

    words and keywords in js

---
+++ JS-BASICS/word_keword.js
@@ -0,0 +1,14 @@
+// name 
+// kular
+// phanka
+// age
+// body_count
+
+// --> These are the words in js which do not have any predefined meaning in the js
+
+// for 
+// else
+// if
+// do
+
+// --> These are the keywords in js which is pre-defined and have the special meaning in the js
