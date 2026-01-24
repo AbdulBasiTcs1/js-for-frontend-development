2dc8bc2534661891a2c3dca85aa371a282d5fe60
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-24 09:31:12 +0500

    reference and primitive

---
+++ JS-BASICS/types_in_js.js
@@ -0,0 +1,34 @@
+// Types in JS
+// Primivitve and Reference 
+
+// Primivtives : 
+// string
+// number
+// boolean
+// undefined
+// null
+// symbol
+// bigint
+
+let a = 10;
+let b = a;
+b = 20;
+
+console.log(a); // 10
+console.log(b); // 20
+
+
+// Refernces : 
+// object
+// array
+// function
+
+let x = { name: "Ali" };
+let y = x;
+
+y.name = "Ahmed";
+
+console.log(x.name); // Ahmed
+
+
+
