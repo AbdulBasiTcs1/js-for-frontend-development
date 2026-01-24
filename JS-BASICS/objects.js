7bb1011b63160ea28abe3122a9e82e95a870f43e
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-24 10:33:29 +0500

    OBJECTS AND THERE PROPERTIES AND VALUES , NESTED OBJECTS ALL COVERED IN JAVASCRIPT

---
+++ JS-BASICS/objects.js
@@ -0,0 +1,28 @@
+// What is an Object?
+
+// An object is a collection of key-value pairs.
+// Keys are called properties, values can be any type.
+// Objects help us organize data in a structured way.
+
+let person = {
+    name: "Ali",
+    age: 25,
+    isStudent: true
+};
+
+console.log(person);
+// out put
+// { name: 'Ali', age: 25, isStudent: true }
+
+// dot notation : 
+// console.log(person.name); // Ali
+// console.log(person.age);  // 25
+
+// BRACKET NOTATION 
+// console.log(person["name"]); // Ali
+
+// etc etc all cleared about the concept of the objects in js
+
+
+
+
