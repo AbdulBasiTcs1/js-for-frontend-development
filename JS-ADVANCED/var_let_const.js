c17a6221f4857008190574803da04ddbc159373b
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-25 21:39:36 +0500

    learning js theor

---
+++ JS-ADVANCED/var_let_const.js
@@ -0,0 +1,10 @@
+// Difference b/w var, let and const 
+// 1.
+// in es5 we was having only "var" 
+// and 
+// in es6 we have the let and const also
+
+// we use both in the js because both are the versions of the JS
+
+
+//2.
