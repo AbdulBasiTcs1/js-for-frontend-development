8b72f218d0dd1850b74bd607e9a444cfb09b75bb
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-30 09:03:50 +0500

    variables

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+// var (old way – function scoped)
+var name = "Basit";
+console.log(name);
+
+// let (block scoped – preferred)
+let age = 21;
+age = 22;
+console.log(age);
+
+// const (block scoped – cannot be reassigned)
+const country = "Pakistan";
+console.log(country);
