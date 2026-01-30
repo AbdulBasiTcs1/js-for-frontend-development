109aef50f0a276e7af1c42aa8741c28a83782dfc
AbdulBasiTcs1 <bearnwith@gmail.com>
2026-01-30 09:36:32 +0500

    coverison of the data types

---
+++ 03_conversion.js
@@ -0,0 +1,33 @@
+// Datatype conversion
+
+// JavaScript converts types automatically.
+let result1 = "5" + 2;
+let result2 = "5" - 2;
+
+console.log(result1); // Output: 52
+console.log(result2); // Output: 3
+
+// Developer converts the data type.
+
+// String -> Number
+let age = "21";
+
+console.log(Number(age));  // Output: 21
+console.log(parseInt(age)); // Output: 21
+
+// Number->String
+let score = 100;
+
+console.log(String(score)); // Output: "100"
+
+// Boolean Conversion 
+console.log(Boolean(1));   // Output: true
+console.log(Boolean(0));   // Output: false
+console.log(Boolean(""));  // Output: false
+console.log(Boolean("ABDUL BASIT"))
+
+// Conversion among the JS DataTypes
+
+
+
+
